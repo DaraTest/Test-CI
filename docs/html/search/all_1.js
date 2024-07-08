b451814c71f009c427bdb5b67398d1bcbf22f743
DaraTest <anasalzoabi54@gmail.com>
2024-07-08 18:15:32 +0200

    Add Doxygen configuration and GitHub Actions workflow for documentation

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['matrixoperations_0',['MatrixOperations',['../class_matrix_operations.html',1,'']]],
+  ['matrixtolatexexporter_1',['MatrixToLaTeXExporter',['../namespace_matrix_to_la_te_x_exporter.html',1,'']]],
+  ['matrixtolatexexporter_3a_3aproperties_2',['Properties',['../namespace_matrix_to_la_te_x_exporter_1_1_properties.html',1,'MatrixToLaTeXExporter']]]
+];
---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['form1_0',['Form1',['../class_matrix_to_la_te_x_exporter_1_1_form1.html',1,'MatrixToLaTeXExporter']]],
+  ['formulaoperations_1',['FormulaOperations',['../class_formula_operations.html',1,'']]]
+];
